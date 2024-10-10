9bc97cd5111f5410dc5bae766a9ad80b8bff657d
Sabit <ceferovsabit999@gmail.com>
2024-10-10 23:17:38 +0400

    added Task1 and Task2

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿string evens = "3 ve 5 qalıqsız bölünen ededler: ";
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 5 == 0)
+
+        if (i % 3 == 0)
+        {
+            evens += i;
+            evens += ", ";
+        }
+}
+Console.WriteLine(evens);
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿var ortalama = 86;
+if (ortalama <= 65)
+{
+    Console.WriteLine("Kesildiniz.");
+}
+else if (ortalama <= 85)
+{
+    Console.WriteLine("Adi Diplom.");
+}
+else if (ortalama <= 95)
+{
+    Console.WriteLine("Honour.");
+}
+else if (ortalama >= 95)
+{
+    Console.WriteLine("High Honour.");
+}
