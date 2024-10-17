ab2f71cc428b323723762efaeb59e1cca210e177
Sabit <ceferovsabit999@gmail.com>
2024-10-17 23:56:01 +0400

    added tasks

---
+++ Taskss/Taskss/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.Design;
+
+namespace Taskss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //Console.WriteLine("Ededi daxil edin: ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //int count = 0;
+            //if (num <= 1)
+            //{
+            //Console.WriteLine("Bu eded ne sade ededdir,nede murekkeb ededdir.");
+            //}
+            //else
+            //{
+                //for (int i = 1; i <= num; i++)
+                //{
+                    //if (num % i == 0) 
+                    //{
+                      //count++;
+                    //}                                       
+                //}
+                //if (count > 2)
+                //{
+                    //Console.WriteLine($"{num} bu eded murekkeb ededdir.");
+                //}
+                //else 
+                //{
+                    //Console.WriteLine($"{num} bu eded sade ededdir.");                
+                //}
+            //}
+            #endregion
+
+            #region Task2
+            //Console.WriteLine("Ededi daxil edin: ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //int count = 0;
+
+            //while (num > 0)
+            //{
+            //num /= 10;
+            //count++;
+            //}
+            //Console.WriteLine($"Ededin mertebelerinin sayi: {count} ");
+            #endregion
+        }
+    }
+}
